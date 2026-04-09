cd3e8e78d6d23a50f00bb4e8bd564b183b729f37
Grégory Viguier <git@screenfeed.fr>
2026-04-09 17:58:41 +0200

    Use `PLL_Language::get_predefined_flag()`

--- src/settings/view-tab-lang.php
+++ src/settings/view-tab-lang.php
@@ -130,7 +130,7 @@ defined( 'ABSPATH' ) || exit;
 								printf(
 									'<option value="%s" data-flag-html="%s"%s>%s</option>' . "\n",
 									esc_attr( $code ),
-									esc_html( PLL_Language::get_flag_html( PLL_Language::get_flag_information( $code ) ) ),
+									esc_html( PLL_Language::get_predefined_flag( $code ) ),
 									selected( isset( $edit_lang->flag_code ) && $edit_lang->flag_code === $code, true, false ),
 									esc_html( $label )
 								);
