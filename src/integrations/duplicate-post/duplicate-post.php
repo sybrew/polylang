db6cb8571cb5d79a7d639fe546af481d36e020c8
Chouby <fred@wp-syntex.com>
2026-04-20 16:00:25 +0200

    Fix conflict with Yoast Duplicate Post (#1844)

--- src/integrations/duplicate-post/duplicate-post.php
+++ src/integrations/duplicate-post/duplicate-post.php
@@ -16,6 +16,9 @@ class PLL_Duplicate_Post {
 	 */
 	public function init() {
 		add_filter( 'option_duplicate_post_taxonomies_blacklist', array( $this, 'taxonomies_blacklist' ) );
+		add_filter( 'pll_copy_post_metas', array( $this, 'exclude_post_metas' ) );
+
+		add_action( 'duplicate_post_after_rewriting', array( $this, 'after_rewriting' ), 20, 2 );
 	}
 
 	/**
@@ -33,5 +36,43 @@ class PLL_Duplicate_Post {
 
 		$taxonomies[] = 'post_translations';
 		return $taxonomies;
+	}
+
+	/**
+	 * Exclude Duplicate Post metas from the copy/synchronization.
+	 *
+	 * This avoids synchronized posts to be deleted when using "Rewrite & Republish".
+	 *
+	 * @since 3.9
+	 *
+	 * @param string[] $keys List of meta keys.
+	 * @return string[]
+	 */
+	public function exclude_post_metas( $keys ): array {
+		$to_remove = array(
+			'_dp_original',
+			'_dp_is_rewrite_republish_copy',
+			'_dp_has_rewrite_republish_copy',
+			'_dp_creation_date_gmt',
+		);
+
+		return array_diff( $keys, $to_remove );
+	}
+
+	/**
+	 * Fixes the translations group just after a post is republished.
+	 *
+	 * @since 3.9
+	 *
+	 * @param int $copy_id The copy's ID.
+	 * @param int $post_id The original post's ID.
+	 * @return void
+	 */
+	public function after_rewriting( $copy_id, $post_id ): void {
+		$language     = pll_get_post_language( $post_id );
+		$translations = pll_get_post_translations( $post_id );
+
+		$translations[ $language ] = $post_id;
+		pll_save_post_translations( $translations );
 	}
 }
